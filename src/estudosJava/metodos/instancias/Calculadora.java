e29caf1ed5db50cfeda8e7068f6094f309bf01b5
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-03-21 18:49:41 -0300

    Criei um método de instância com parâmetros

---
+++ src/estudosJava/metodos/instancias/Calculadora.java
@@ -0,0 +1,15 @@
+package estudosJava.metodos.instancias;
+
+public class Calculadora {
+	
+	public void somar(int a, int b) {
+		int resultado = a + b;
+		System.out.println("A soma de " + a + " e " + b + " é: " + resultado);
+	}
+	
+	public static void main(String[] args) {
+		Calculadora calc = new Calculadora();
+		
+		calc.somar(5, 10);
+	}
+}	
