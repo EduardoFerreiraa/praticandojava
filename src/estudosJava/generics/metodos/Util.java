5c74159a197054c1264c6270227e6fdd94aaa028
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-04-25 17:58:59 -0300

    Exemplo de método genérico

---
+++ src/estudosJava/generics/metodos/Util.java
@@ -0,0 +1,8 @@
+package estudosJava.generics.metodos;
+
+public class Util {
+	
+	public static <T> void imprimirValor(T valor) {
+		System.out.println("Valor: " + valor);
+	}
+}
---
+++ src/estudosJava/generics/metodos/test/UtilTest.java
@@ -0,0 +1,12 @@
+package estudosJava.generics.metodos.test;
+
+import estudosJava.generics.metodos.Util;
+
+public class UtilTest {
+	public static void main(String[] args) {
+		Util.imprimirValor(100);
+		Util.imprimirValor("Eduardo");
+		Util.imprimirValor(100.90);
+		Util.imprimirValor(true);
+	}
+}
