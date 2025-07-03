72dda8bc544860281f9d66a5ebfd1fbc4a8ef9fc
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-07-02 21:52:41 -0300

    Obtendo a hora de agora com LocalTime

---
+++ src/praticas/datas/ExemploHora.java
@@ -0,0 +1,12 @@
+package praticas.datas;
+
+import java.time.LocalTime;
+
+public class ExemploHora {
+	public static void main(String[] args) {
+		LocalTime agora = LocalTime.now();
+		System.out.println(agora);
+		
+	}
+	
+}
