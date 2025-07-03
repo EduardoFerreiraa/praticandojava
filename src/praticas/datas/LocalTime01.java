a33998fe6e4d314194f1d83364cb36829b1fa840
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-07-02 21:55:24 -0300

    Criando um horario especifico

--- src/praticas/datas/LocalTime01.java
+++ src/praticas/datas/LocalTime01.java
@@ -2,11 +2,11 @@ package praticas.datas;
 
 import java.time.LocalTime;
 
-public class ExemploHora {
+public class LocalTime01 {
 	public static void main(String[] args) {
+		
 		LocalTime agora = LocalTime.now();
 		System.out.println(agora);
-		
 	}
 	
 }
---
+++ src/praticas/datas/LocalTime02.java
@@ -0,0 +1,11 @@
+package praticas.datas;
+
+import java.time.LocalTime;
+
+public class LocalTime02 {
+	public static void main(String[] args) {
+		// Cria um horário específico: 15 horaas, 30 minutos e 0 segundos
+		LocalTime horario = LocalTime.of(15, 30, 0);
+		System.out.println("Horário escolhido: " + horario);
+	}
+}
