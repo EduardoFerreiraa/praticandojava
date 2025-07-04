9dc5f034ebcf6afa28273d52d4c8bc8827941d45
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-07-04 18:30:14 -0300

    Praticando LocalDateTIme

---
+++ src/estudosJava/datas/localDateTime/LocalDateTime01.java
@@ -0,0 +1,10 @@
+package estudosJava.datas.localDateTime;
+
+import java.time.LocalDateTime;
+
+public class LocalDateTime01 {
+	public static void main(String[] args) {
+		LocalDateTime dataEhoraDeAgora = LocalDateTime.now();
+		System.out.println(dataEhoraDeAgora);
+	}
+}
