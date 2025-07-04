f71edae8f0f4ced6a84a76b7f7c3d9cd7dfc9ddf
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-07-04 18:42:33 -0300

    Praticando data e horas com LocalDateTime.of()

---
+++ src/praticas/datas/localDateTime/LocalDateTime02.java
@@ -0,0 +1,10 @@
+package praticas.datas.localDateTime;
+
+import java.time.LocalDateTime;
+
+public class LocalDateTime02 {
+	public static void main(String[] args) {
+		LocalDateTime dataEhoras = LocalDateTime.of(2025, 04, 07, 18, 32);
+		System.out.println("Data e hora que escolhi: " + dataEhoras);
+	}
+}
