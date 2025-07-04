b01485c919668c0cc408f670d0edca617a8622df
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-07-04 18:36:53 -0300

    Obtendo data e hora escolhida com LocalDateTime.of

---
+++ src/estudosJava/datas/localDateTime/LocalDateTime02.java
@@ -0,0 +1,10 @@
+package estudosJava.datas.localDateTime;
+
+import java.time.LocalDateTime;
+
+public class LocalDateTime02 {
+	public static void main(String[] args) {
+		LocalDateTime dataHora = LocalDateTime.of(2025, 04, 07, 18, 32);
+		System.out.println("Data e hora escolhida: " + dataHora);
+	}
+}
