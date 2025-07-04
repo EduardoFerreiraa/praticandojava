914e346748afb4cf1d8d05efd965524f949e376c
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-07-04 18:21:49 -0300

    Praticando LocalDate.of()

---
+++ src/praticas/datas/LocalDate01.java
@@ -0,0 +1,10 @@
+package praticas.datas;
+
+import java.time.LocalDate;
+
+public class LocalDate01 {
+	public static void main(String[] args) {
+		LocalDate agora = LocalDate.now();
+		System.out.println(agora);
+	}
+}
---
+++ src/praticas/datas/LocalDate02.java
@@ -0,0 +1,10 @@
+package praticas.datas;
+
+import java.time.LocalDate;
+
+public class LocalDate02 {
+	public static void main(String[] args) {
+		LocalDate dataAnoNovo = LocalDate.of(2026, 1, 1);
+		System.out.println(dataAnoNovo);
+	}
+}
