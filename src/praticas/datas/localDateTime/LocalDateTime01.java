7cf1ea2bd888c7645c22c3391b1a341ab3a3413d
Eduardoalmeida77 <eduardoferreiraalmeida20@gmail.com>
2025-07-04 18:28:07 -0300

    LocalDateTime, data e horas atuais

---
+++ src/praticas/datas/localDateTime/LocalDateTime01.java
@@ -0,0 +1,10 @@
+package praticas.datas.localDateTime;
+
+import java.time.LocalDateTime;
+
+public class LocalDateTime01 {
+	public static void main(String[] args) {
+		LocalDateTime agora = LocalDateTime.now();
+		System.out.println("data e hora atuais: " + agora);
+	}
+}
